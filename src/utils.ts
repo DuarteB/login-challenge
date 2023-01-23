afd94163211132c805ed342744367c08383ecbd9
unknown <eduarte.dev@gmail.com>
2023-01-22 22:59:11 -0300

    Adding challenge files

---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+export type User ={
+  email: string,
+  password: string
+}
+
+const login = ({ email, password }: User) => {
+  const delay = (0.7 + Math.random() * 2) * 1000;
+
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      if(password === 'password123' && email == 'eduardo@gmail.com'){
+        resolve(
+          alert('Login efetuado!')
+        );
+      } else {
+        reject({
+          message: 'Email or Password wrong.'
+        })
+      }
+    }, delay)
+  })
+}
+
+export default login;
